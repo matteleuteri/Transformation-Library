1ef2797310c5f5c29c3eddc0fbdd2df8019bb721
Matthew Eleuteri <meleuteri2@gmail.com>
2022-09-07 19:37:28 -0400

    .

--- code/GameObject.cpp
+++ code/GameObject.cpp
@@ -1,11 +1,9 @@
 #include "headers/GameObject.h"
 
 GameObject::GameObject(AnimationController *animationController, Transform transform, float xSpeed): animationController(animationController), x(x), y(y), xSpeed(xSpeed)
-//{
-//    animation = animationController->animationMap["EXAMPLE1"];
-    // D2D1_SIZE_F size = animation->bitmaps[0]->GetSize();
-    // width = size.width;
-    // height = size.height;
-    // ySpeed = 0;
-//}
+{
+    animation = animationController->animationMap["EXAMPLE1"];
+    D2D1_SIZE_F size = animation->bitmaps[0]->GetSize();
+    std::array<std::array<float>> = { { 1.0f, 1.0f, 1.0f } };
+}
 
